ace1860c72969f6f0e9a9cfbfe109c83be5b54b8
TSShrol <shrolts@gmail.com>
2023-10-04 20:23:06 +0300

    add binding properties and events

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -7,6 +7,7 @@ import { IMovie } from './movie';
   styleUrls: ['./movies.component.css']
 })
 export class MoviesComponent {
+  
   movies: IMovie[] = [
     {
       title: "Dune",
@@ -21,7 +22,7 @@ export class MoviesComponent {
       description: "John, an ex-CIA officer, is entrusted with the responsibility of safeguarding an entrepreneur's daughter. When the girl gets kidnapped, John vows to seek revenge.",
       year: 2004,
       duration: { hours: 2, minutes: 26 },
-      isCurrent: true,
+      isCurrent: false,
       genres: ["Drama", "Craim", "Action"]
     },
     {
@@ -29,10 +30,19 @@ export class MoviesComponent {
       description: "Commodus takes over power and demotes Maximus, one of the preferred generals of his father, Emperor Marcus Aurelius. As a result, Maximus is relegated to fighting till death as a gladiator.",
       year: 2000,
       duration: { hours: 2, minutes: 35 },
-      isCurrent: true,
+      isCurrent: false,
       genres: ["Drama", "Action","History"]
     }
-
   ];
+    //methods
+    changeCurrentStatus(movie:IMovie):void{
+      movie.isCurrent=!movie.isCurrent;
+    }
+
+    deleteMovie(movie: IMovie):void{
+      let index=this.movies.indexOf(movie);
+      this.movies.splice(index,1);
+    }
+
 
 }
